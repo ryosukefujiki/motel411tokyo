cb0cb9cafadebf0a4fecf59407f744c5c4652af1
ryosukefujiki <r.fujiki.lit@gmail.com>
2019-12-02 00:30:09 +0900

    Add the redirect program

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,15 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        motel411tokyo
-      </h1>
-      <h2 class="subtitle">
-        This site is motel 4111 tokyo site
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
+import Vue from 'Vue'
+export default Vue.extend({
+  fetch({ redirect }) {
+    redirect('https://motel411.base.shop/')
   }
-}
+})
 </script>
 
 <style>
